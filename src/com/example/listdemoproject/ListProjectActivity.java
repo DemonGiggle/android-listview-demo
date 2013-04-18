e61d187b6eef4d29f1361b36b262b1d8f7916b4d
giggle <giggle@zillians.com>
2013-04-18 19:29:35 +0800

    initial commit

---
+++ src/com/example/listdemoproject/ListProjectActivity.java
@@ -0,0 +1,105 @@
+package com.example.listdemoproject;
+
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+class Item {
+	
+	public Item(String title, String caption, boolean isChecked) {
+		this.title = title;
+		this.caption = caption;
+		this.isChecked = isChecked;
+	}
+	
+	public String title;
+	public String caption;
+	public boolean isChecked;
+}
+
+class ItemAdapter extends ArrayAdapter<Item> {
+
+	private ArrayList<Item> objects;
+	
+	public ItemAdapter(Context context, int textViewResourceId, ArrayList<Item> objects) {
+		super(context, textViewResourceId, objects);
+		this.objects = objects;
+	}	
+	
+	@Override
+	public boolean areAllItemsEnabled() {
+		return true;
+	}
+	
+	@Override
+	public int getCount() {
+		return objects.size();
+	}
+	
+	@Override
+	public boolean isEnabled(int position) {
+		return true;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		if (v == null) {
+			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			v = inflater.inflate(R.layout.list_twoline_item_checkbox, null);
+		}
+		
+		Item item = objects.get(position);
+		((TextView) v.findViewById(R.id.list_twoline_caption)).setText(item.caption);
+		((TextView) v.findViewById(R.id.list_twoline_title)).setText(item.title);
+		((CheckBox) v.findViewById(R.id.checkBoxEnable)).setChecked(item.isChecked);
+		
+		return v;
+	}
+}
+
+public class ListProjectActivity extends ListActivity {
+
+	private ItemAdapter mAdapter;
+	private ArrayList<Item> mItems;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_project);
+
+		mItems = new ArrayList<Item>();
+		mItems.add(new Item("Tennis", "Mark", false));
+		mItems.add(new Item("Basketball", "Donnis", true));
+		mItems.add(new Item("Flybee", "Allis", true));
+		
+		mAdapter = new ItemAdapter(this, R.layout.list_twoline_item_checkbox, mItems);
+		
+		setListAdapter(mAdapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.list_project, menu);
+		return true;
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {		
+		Item item = (Item) l.getItemAtPosition(position);
+		Toast.makeText(this, "Click: " + item.caption, Toast.LENGTH_SHORT).show();
+	}
+
+}
